bbeacffa228fb76bd9ad8b1153a4306e42510810
Aravind071845 <727721euec011@skcet.ac.in>
2023-03-22 21:25:46 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+       <h1>
+         Let we see the output of JAVASCRIPT
+       </h1>
+       <button onClick={window['alertMsg']}>Click to call msg alert</button>
+      </header>
+  );
+}
+
+export default App;
